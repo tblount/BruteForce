b70600160e891a800659f3c62640df4ba5963e43
T.Blount <tblountcs@gmail.com>
2021-04-22 16:51:36 -0400

    Player is able to present name and home village,AB#159

--- src/escaperooms/joninexams/JoninExams.java
+++ src/escaperooms/joninexams/JoninExams.java
@@ -6,6 +6,8 @@ import escaperooms.application.Traveler;
 import escaperooms.application.User;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class JoninExams extends EscapeRoom {
     Traveler traveler;
@@ -38,4 +40,21 @@ public class JoninExams extends EscapeRoom {
         traveler.jump(room);
     }
 
+    public static void main(String[] args) {
+
+        ArrayList<String> villages = new ArrayList<String>();
+        villages.add("Hidden Leaf");
+        villages.add("The Village Hidden in the Waterfall");
+        villages.add("The Village Hidden in the Stones");
+        villages.add("The Village Hidden in the Sand");
+        villages.add("The Village Hidden in the Rain");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello there! Welcome to the Jonin Exams");
+        System.out.println("What is your name?");
+        String name = input.nextLine();
+        System.out.println("What village are you from?" + villages);
+        String home = input.nextLine();
+        System.out.println(home);
+    }
 }
