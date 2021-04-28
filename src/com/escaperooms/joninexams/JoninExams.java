cce49bd0e2f3d50ff59a673e0ea96bbc3e369c28
T.Blount <tblountcs@gmail.com>
2021-04-27 23:21:12 -0400

    Player can present name and home village,AB#159

--- src/com/escaperooms/joninexams/JoninExams.java
+++ src/com/escaperooms/joninexams/JoninExams.java
@@ -23,6 +23,7 @@ public class JoninExams extends EscapeRoom {
         this.traveler = traveler;
         this.user = traveler.getUser();
         escapeRoom.getEscapeRooms().remove("Jonin Exams");
+        welcome();
     }
 
     @Override
@@ -40,22 +41,17 @@ public class JoninExams extends EscapeRoom {
         traveler.jump(room);
     }
 
-//    public static void main(String[] args) {
-//
-//        Prompter input = new Prompter(new Scanner(System.in));
-//
-//        ArrayList<String> villages = new ArrayList<String>();
-//        villages.add("Hidden Leaf");
-//        villages.add("The Village Hidden in the Waterfall");
-//        villages.add("The Village Hidden in the Stones");
-//        villages.add("The Village Hidden in the Sand");
-//        villages.add("The Village Hidden in the Rain");
-//
-////        Scanner input = new Scanner(System.in);
-//        System.out.println("Hello there! Welcome to the Jonin Exams");
-//        String name = input.prompt("What is your name?");
-////        System.out.println(name);
-//        String home = input.prompt("What village are you from?" + villages);
-////        System.out.println(home);
-//    }
+    void welcome() {
+
+        Prompter input = new Prompter(new Scanner(System.in));
+
+        ArrayList<String> villages = new ArrayList<String>();
+        villages.add("Hidden Leaf");
+        villages.add("The Village Hidden in the Waterfall");
+        villages.add("The Village Hidden in the Stones");
+
+        System.out.println("Hello there! Welcome to the Jonin Exams");
+        String name = input.prompt("What is your name?");
+        String home = input.prompt("OK..." + name + " What village are you from? " + villages);
+    }
 }
