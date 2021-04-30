d074b62e5073349f6aa50d66d172eb4d4f01a4f4
Mr Windows SDE <f3153808@avmtk.com>
2021-04-30 16:36:01 -0500

    ansi support

--- src/com/escaperooms/client/Client.java
+++ src/com/escaperooms/client/Client.java
@@ -3,11 +3,14 @@ package com.escaperooms.client;
 import com.escaperooms.application.EscapeRoom;
 import com.escaperooms.application.Traveler;
 import com.escaperooms.application.User;
+import org.fusesource.jansi.AnsiConsole;
+
 import java.io.IOException;
 
 class Client {
 
     public static void main(String[] args) throws IOException {
+        AnsiConsole.systemInstall();
         User user = new User();
         EscapeRoom escapeRoom = new EscapeRoom();
         escapeRoom.generateEscapeRooms(escapeRoom);
