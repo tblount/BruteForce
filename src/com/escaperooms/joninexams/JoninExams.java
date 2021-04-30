acc3efecb004b8edbb4e097e7e2fb4e695b9b98f
T.Blount <tblountcs@gmail.com>
2021-04-30 10:28:41 -0400

    Jonin Exams is clean and more readable,AB#302

--- src/com/escaperooms/joninexams/JoninExams.java
+++ src/com/escaperooms/joninexams/JoninExams.java
@@ -25,6 +25,47 @@ public class JoninExams extends EscapeRoom {
         escapeRoom.getEscapeRooms().remove("Jonin Exams");
         welcome();
     }
+    void welcome() {
+
+        Ninja fighter1 = new Ninja("Kakashi", 10);
+        Ninja fighter2 = new Ninja("Gaara", 10);
+
+        Prompter input = new Prompter(new Scanner(System.in));
+
+        ArrayList<String> villages = new ArrayList<String>();
+        villages.add("1: Hidden Leaf Village");
+        villages.add("2: The Village Hidden in the Stones");
+
+        System.out.println("=====================================================================================");
+        System.out.println("Hello " + user.getName() + "\n Wow I use to know a legendary Ninja by that name!!");
+        System.out.println("=====================================================================================");
+        System.out.println("Welcome to the Jonin Exams, there will be some tough opponents today!");
+        System.out.println("=====================================================================================");
+        System.out.println("Ok here is a list of villages that can compete in the Jonin Exams today");
+        System.out.println("=====================================================================================");
+        System.out.println(villages.get(0));
+        System.out.println(villages.get(1));
+
+        System.out.println("=====================================================================================");
+        String home = input.prompt("What village are you from?");
+        System.out.println("=====================================================================================");
+
+        switch (home) {
+            case "1":
+                System.out.println("Looks like you wil facing the Legendary..." + fighter1.getNinjaName());
+                break;
+            case "2":
+                System.out.println("Ok..." + user.getName() + " You will be fighting " + fighter2.getNinjaName());
+                break;
+            default:
+                System.out.println("That village is not allowed to participate in the exams.");
+        }
+        System.out.println("=====================================================================================");
+
+        user.addItem("leaf headband");
+        System.out.println(" ");
+        System.out.println("Have fun on journey!!");
+    }
 
     @Override
     public void terminate() {
@@ -40,33 +81,6 @@ public class JoninExams extends EscapeRoom {
         traveler.menu();
     }
 
-    void welcome() {
-
-        Ninja fighter1 = new Ninja("Kakashi", 10);
-        Ninja fighter2 = new Ninja("Gaara", 10);
-
-        Prompter input = new Prompter(new Scanner(System.in));
-
-        ArrayList<String> villages = new ArrayList<String>();
-        villages.add("hidden leaf village");
-        villages.add("The Village Hidden in the Stones");
-
-        System.out.println("Hello " + user.getName() + "\n Wow I use to know a legendary Ninja by that name!!");
-        System.out.println(" ");
-        System.out.println("Ok here is a list of villages that can compete in the Jonin Exams today -------> " + villages);
-
-        String home = input.prompt("...What village did you say you're from? ");
-
-        if (home.equals("hidden leaf village")) {
-            System.out.println("Looks like you will be battling..." + fighter1.getNinjaName());
-        } else
-            System.out.println("Looks like you will go against the mighty! " + fighter2.getNinjaName());
-
-        user.addItem("leaf headband");
-        System.out.println(" ");
-        System.out.println("Have fun on journey!!");
-        jump();
-    }
     @Override
     public String getName() {
         return "Jonin Exams";
